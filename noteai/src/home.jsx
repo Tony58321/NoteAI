964ea21ec52b244559d0b7edce84f685b2d57471
Antonio Rodriguez <arodriguez588.ar@gmail.com>
2025-04-05 16:11:16 -0700

    Login to Firebase using email and password

--- noteai/src/home.jsx
+++ noteai/src/home.jsx
@@ -2,7 +2,7 @@ export default function Home({user, logout}) {
     return <>
         <h1>NoteAI</h1>
         <h3>Slogan here</h3>
-        <p>welcome, {String(user)}</p>
+        <p>welcome, {String(user.email)}</p>
         <button onClick={() => null}>New Notes</button>
         <button onClick={logout}>Logout</button>
     </>
