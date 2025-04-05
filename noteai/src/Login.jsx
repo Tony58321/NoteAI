d0b6e8afaab32b25984755d930f44cf0d3f88f8f
granjame <jlg20045@gmail.com>
2025-04-05 14:27:11 -0700

    Added login boxes and placeholder function

--- noteai/src/Login.jsx
+++ noteai/src/Login.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import LogInUser from '../utilities/logInUser'
 
 export default function Login() {
 
     return (
       <>
         <h1>NoteAI</h1>
-        <></>
+        <label for='Username'>Username</label>
+        <input type='text' name='Username' id='usernameInput'></input>
+        <br/>
+        <label for='password'>Password:</label>
+        <input type='text' name='password' id='passwordInput'></input>
+        
+        <button onClick={() => LogInUser(
+          document.getElementById('usernameInput').value,
+          document.getElementById('passwordInput').value
+          )}>Log in</button>
       </>
     )
   }
---
+++ noteai/utilities/logInUser.js
@@ -0,0 +1,4 @@
+export default function LogInUser(username, password) {
+    console.log(`log in function called for user ${username} with password ${password}`)
+    return;
+}
