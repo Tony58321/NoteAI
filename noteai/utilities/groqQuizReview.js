1e8ac9084f7eb7f6c64e3caffad937ace7c47fcc
granjame <jlg20045@gmail.com>
2025-04-06 11:46:26 -0700

    Tweaked some prompts for the quiz

--- noteai/utilities/groqQuizReview.js
+++ noteai/utilities/groqQuizReview.js
@@ -5,10 +5,10 @@ export default async function GroqQuizReview(questions) {
     const prompt = `
         You will be presented with a lists of questions, reference notes, and answers to check.
         Look over how accurate the answers are to the questions and reference notes.
-        Give a brief three sentence summary of the following things.
-        1. A statement of the general accuracy of the given answers in relation to the questions and the reference notes.
-        2. Mention any specific areas or topics that the answers need to be improved on or did especially well on.
-        2. An encouraging remark for the person answering the questions.
+        Give a brief three sentence summary with the following considerations.
+        * An overview of the general accuracy of the given answers in relation to the questions and the reference notes.
+        * Mention of any specific areas or topics that the answers need to be improved on or did especially well on.
+        * An encouraging remark for the person answering the questions.
         
         Here are the questions:
         ${questions.map((question, i) => (
