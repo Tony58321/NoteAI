69335d29544eba9e61801d8aaf44b61c29da3cea
lizzy-gs <stahlkee@oregonstate.edu>
2025-04-05 21:12:31 -0700

    commit

--- noteai/src/Landing.jsx
+++ noteai/src/Landing.jsx
@@ -4,7 +4,7 @@ import './Landing.css'
 export default function Landing({setLoginPage}) {
     return (
         <>
-            <h1 id="welcome">Welcome to NoteAI</h1>
+            <h1 id="welcome">NoteAI</h1>
             <h4 id="slogan">Notes made easy, thinking made deep.</h4>
             <button id="toLogin"
                 onClick={setLoginPage}
