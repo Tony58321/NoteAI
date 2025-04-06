e73c68e08de67dcf7ab525cd8a08ddc0022fa611
Aaron-40 <140855821+Aaron-40@users.noreply.github.com>
2025-04-06 00:42:45 -0700

    substantial work on flashcards

--- noteai/utilities/CallGroq.js
+++ noteai/utilities/CallGroq.js
@@ -11,7 +11,7 @@ export default async function callGroq(inputString){
 
 
     //returns the generated flash cards
-    let quizCards = await generateQuiz()
+    let quizCards = await generateQuiz(inputString)
     console.log(quizCards);
     return (quizCards);
 }
@@ -19,9 +19,10 @@ export default async function callGroq(inputString){
 
 
 
+export async function generateQuiz(user_notes){
 
-export async function generateQuiz(){
 
+user_notes = "insert long notes here";
 
 let instructions = "You are going to generate a flashcard quiz in the following format. For each question write a division bar like this write before starting the question: " 
 + "/-----------------------------------------------------------------------/"
@@ -29,7 +30,7 @@ let instructions = "You are going to generate a flashcard quiz in the following 
 +"*******************"
 +" then write the answer to the question. Then you can start writing the next question + answer combo. "
 
-let string = "Write 10 questions about: U.S. state capitals."
+let string = "Write a reasonable number of question to cover these notes that a student has written: " + user_notes;
 
 
 let comboString = instructions + string;
@@ -44,7 +45,7 @@ let quizQuestionArray = [];
 
 
 
-/*THIS SECTION WILL REQUIRE SOME WORK TO ENSURE ALL EDGE CASES ARE COVERED BUT SHOULD BE FINE FOR NOW*/
+//THIS SECTION WILL REQUIRE SOME WORK TO ENSURE ALL EDGE CASES ARE COVERED BUT SHOULD BE FINE FOR NOW
 
 // Split the string by newline character to get an array of lines
 let lines = result.split('\n');
@@ -100,6 +101,95 @@ quizQuestionArray.push(cardItem);
 
 console.log(quizQuestionArray);
 return quizQuestionArray;
+}
 
+
+
+
+
+
+
+//Old function version, see above for current version 
+/* 
+export async function generateQuiz(){
+
+
+let instructions = "You are going to generate a flashcard quiz in the following format. For each question write a division bar like this write before starting the question: " 
++ "/-----------------------------------------------------------------------/"
++ " Then write the question. After the question write another sub-division line like this: " 
++"*******************"
++" then write the answer to the question. Then you can start writing the next question + answer combo. "
+
+let string = "Write 10 questions about: U.S. state capitals."
+
+
+let comboString = instructions + string;
+
+
+let result = await askGroqPrompt(comboString);
+console.log(result);
+
+
+let quizQuestionArray = [];
+
+
+
+
+//THIS SECTION WILL REQUIRE SOME WORK TO ENSURE ALL EDGE CASES ARE COVERED BUT SHOULD BE FINE FOR NOW
+
+// Split the string by newline character to get an array of lines
+let lines = result.split('\n');
+// Iterate through the array of lines
+
+let questionSavingMode = false;
+let questionString = "";
+let answerString = "";
+for (let i = 0; i < lines.length; i++) {
+
+
+  if (lines[i] == "/-----------------------------------------------------------------------/" ){
+
+    //Record previous Q+A combo before starting
+    if(questionString != ""){
+
+
+      let cardItem = {
+        question: questionString,
+        answer: answerString
+      };
+      quizQuestionArray.push(cardItem);
+    }
+
+
+    questionSavingMode = true;
+    questionString = "";
+    answerString = "";
+    continue;
+  }
+  else if(lines[i] =="*******************" ){
+    questionSavingMode = false;
+    answerString = "";
+    continue;
+  }
+
+  //currently recording a question
+  if(questionSavingMode == true ){
+    questionString += lines[i];
+  }
+  //currently recording an answer
+  else{
+    answerString += lines[i];
+  } 
 
 }
+//record last trailing Q+A combo 
+let cardItem = {
+  question: questionString,
+  answer: answerString
+};
+quizQuestionArray.push(cardItem);
+
+console.log(quizQuestionArray);
+return quizQuestionArray;
+}
+*/
